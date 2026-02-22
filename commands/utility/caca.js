4fa9b3edd109269194188bd8af1558265ebdac23
AlbanTrincherini <alban.trincherini@epfl.ch>
2026-02-22 12:42:43 +0100

    Remove tutorial commands

--- commands/utility/caca.js
+++
@@ -1,8 +0,0 @@
-const { SlashCommandBuilder } = require('discord.js');
-
-module.exports = {
-	data: new SlashCommandBuilder().setName('caca').setDescription('Replies with Pipi!'),
-	async execute(interaction) {
-		await interaction.reply('Pipi!');
-	},
-};
